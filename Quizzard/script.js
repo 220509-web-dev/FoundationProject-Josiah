f083ea0949b0fd2c3fd533154d7a86801565c326
Josiah Sparks <josiah.sparks78@gmail.com>
2022-05-21 08:59:57 -0500

    Answer hidden until clicked

--- Quizzard/script.js
+++ Quizzard/script.js
@@ -27,6 +27,8 @@ fetch(loc)
 function getRandomInt(max) {
   <!-- Returns integer in range [0,max] meaning 0 or max could be returned -->
   return Math.floor(Math.random() * (max+1));
+  <!-- Could be modified to find random in range with multiple omits -->
+  <!-- That would allow for a size >1 memory int-array that won't be selected -->
 }
 function set_sel(val) {
   window.sel = val;
@@ -41,8 +43,12 @@ function rnd_sel() {
   } else if (window.elems == 1) {set_sel(0);}
 }
 
-function repl_with() {
+function repl_q() {
   rnd_sel();
   document.getElementById('question').innerHTML = window.db_c[window.sel][0];
+  document.getElementById('answer').innerHTML = "<p>Click to see answer</p>";
+  <!-- window.db_c[window.sel][1] -->
+}
+function repl_a() {
   document.getElementById('answer').innerHTML = window.db_c[window.sel][1];
 }
