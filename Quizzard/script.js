424c2729635f39d7c991d2c5e864cb66bb0cb85b
Josiah Sparks <josiah.sparks78@gmail.com>
2022-05-22 18:16:40 -0500

    Implemented card memory

--- Quizzard/script.js
+++ Quizzard/script.js
@@ -3,42 +3,68 @@ const db_raw = [];
 const db_c = [];
 var sel = -1;
 var elems = 0;
+const memory = [];
+var mem_len = 0;
+
 <!-- Fetch content from another webpage -->
 let loc = 'https://raw.githubusercontent.com/220509-web-dev/FoundationProject-Josiah/main/Quizzard/db.html';
 fetch(loc)
   .then(response => response.text())
   .then(data => set_db(data));
 
- function set_db(text) {
-  <!-- sets global variable data_db -->
-  <!-- Runs after fetch completes -->
-  window.data_db = text;
-  window.db_raw = window.data_db.split(";;;;;;;;");
+function set_db(text) {
+ <!-- sets global variable data_db -->
+ <!-- Runs after fetch completes -->
+ window.data_db = text;
+ window.db_raw = window.data_db.split(";;;;;;;;");
 
-  <!-- Set db_c to a blank 2-d array of proper length -->
-  window.elems = window.db_raw.length;
-  window.db_c = [...Array(window.elems)].map(x => Array(2).fill(''));
+ <!-- Set db_c to a blank 2-d array of proper length -->
+ window.elems = window.db_raw.length;
+ window.db_c = [...Array(window.elems)].map(x => Array(2).fill(''));
 
-  for (var i = 0; i < window.elems; i++) {
-    window.db_c[i] = window.db_raw[i].split(";;;;");
-  }
-
+ for (var i = 0; i < window.elems; i++) {
+   window.db_c[i] = window.db_raw[i].split(";;;;");
  }
-function getRandomInt(max) {
-  <!-- Returns integer in range [0,max] meaning 0 or max could be returned -->
-  return Math.floor(Math.random() * (max+1));
-  <!-- Could be modified to find random in range with multiple omits -->
-  <!-- That would allow for a size >1 memory int-array that won't be selected -->
+}
+
+function getRandomInt() {
+  <!-- window.elems is >= 2 -->
+  let max = window.elems-1-memory.length;
+  let rnum = Math.floor(Math.random() * (max+1));
+  <!-- count how many in memory are <= rnum -->
+  var i = 0;
+  var final_rnum = rnum-0;
+  <!-- step -->
+  while (i <= final_rnum) {
+    if (memory.includes(i)) { final_rnum++; }
+    i++;
+  }
+  var adjust = final_rnum - rnum;
+  memory.push(final_rnum);
+  var max_size = Math.floor(window.elems /2);
+  if (memory.length > max_size) { memory.shift(); }
+  console.log("adjust");
+  console.log(adjust);
+  console.log("rnum");
+  console.log(rnum);
+  console.log("memory");
+  console.log(memory.toString());
+  return final_rnum;
 }
 function set_sel(val) {
   window.sel = val;
 }
+
+function remember(val) {
+  var max_size = Math.floor(window.elems /2);
+  if (max_size > 0) {
+    memory.push(val);
+    if (memory.length > max_size) { memory.shift(val); }
+  }
+}
 function rnd_sel() {
   if (window.elems > 1) {
-    var rnd_int = getRandomInt(window.elems-1);
-    while (window.sel == rnd_int) {
-      var rnd_int = getRandomInt(window.elems-1);
-    }
+    var rnd_int = getRandomInt();
     set_sel(rnd_int);
   } else if (window.elems == 1) {set_sel(0);}
 }
