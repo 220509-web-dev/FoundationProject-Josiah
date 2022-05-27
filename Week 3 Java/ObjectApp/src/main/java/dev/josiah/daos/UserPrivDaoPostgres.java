677c9976dad0d177e809aaa5be73ed3b905ae714
Josiah Sparks <josiah.sparks78@gmail.com>
2022-05-27 18:43:40 -0500

    completed user_private table DAO

--- Week 3 Java/ObjectApp/src/main/java/dev/josiah/daos/UserPrivDaoPostgres.java
+++ Week 3 Java/ObjectApp/src/main/java/dev/josiah/daos/UserPrivDaoPostgres.java
@@ -1,6 +1,7 @@
 package dev.josiah.daos;
 
 import dev.josiah.entities.User;
+import dev.josiah.entities.UserPriv;
 import dev.josiah.utils.ConnectionUtil;
 
 import java.sql.*;
@@ -31,9 +32,39 @@ public class UserPrivDaoPostgres implements UserPrivDAO{
     final private String[] c = {c0,c1,c2};
 
     @Override
-    public User createUserInfo(User user) {
+    public void createUserInfo(UserPriv userp) {
         try(Connection conn = ConnectionUtil.getConnection()) {
             String sql = "insert into "+st+" values (";
+            for (int i = 0; i<c.length-1;i++) { sql += "?,"; }
+            sql += "?);";
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setInt(1, userp.getUser_id());
+            ps.setString(2, userp.getSocial_sn());
+            ps.setString(3, userp.getPassword());
+            ps.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public UserPriv getUserInfoById(int id) {
+        try(Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "select * from "+st+" where "+c[0]+" = ? ;";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1,id); // indexed from 1 rather than 0
+            ResultSet rs = ps.executeQuery();
+
+            // Get first record
+            if (rs.next()) {
+                UserPriv userp = new UserPriv();
+                userp.setUser_id(   rs.getInt(c[0]));
+                userp.setSocial_sn( rs.getString(c[1]));
+                userp.setPassword(   rs.getString(c[2]));
+                return userp;
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -41,9 +72,23 @@ public class UserPrivDaoPostgres implements UserPrivDAO{
     }
 
     @Override
-    public User getUserInfoById(int id) {
+    public List<UserPriv> getAllUserInfo() {
         try(Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "select * from "+st+";";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
 
+            List<UserPriv> userps = new ArrayList<UserPriv>();
+            boolean results = false;
+            while (rs.next()) {
+                results = true;
+                UserPriv userp = new UserPriv();
+                userp.setUser_id(   rs.getInt(   c[0]));
+                userp.setSocial_sn( rs.getString(c[1]));
+                userp.setPassword(  rs.getString(c[2]));
+                userps.add(userp);
+            }
+            if (results) return userps;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -51,29 +96,32 @@ public class UserPrivDaoPostgres implements UserPrivDAO{
     }
 
     @Override
-    public List<User> getAllUserInfo() {
+    public UserPriv updateUserInfo(UserPriv userp) {
         try(Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "update "+st+" set ";
+            for (int i=1; i<c.length-1;i++) {sql += c[i]+" = ?,";}
+            sql += c[c.length-1]+" = ? where "+c[0]+"=?;";
+            PreparedStatement ps = conn.prepareStatement(sql);
 
+            ps.setString( 1, userp.getSocial_sn());
+            ps.setString( 2, userp.getPassword());
+            ps.setInt(    3, userp.getUser_id());
+            ps.execute();
+
+            return userp;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return null;
-    }
-
-    @Override
-    public User updateUserInfo(User user) {
-        try(Connection conn = ConnectionUtil.getConnection()) {
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return null;
+        //return null;
     }
 
     @Override
     public void deleteUserInfoById(int id) {
         try(Connection conn = ConnectionUtil.getConnection()) {
-
+            String sql = "delete from "+st+" where "+c[0]+" = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
