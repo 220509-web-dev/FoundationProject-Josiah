8be90e936a8d8cdfd94a9c4fee3e195e2663057b
Josiah Sparks <josiah.sparks78@gmail.com>
2022-05-26 22:15:12 -0500

    modified for non-case-sensitive table names

--- Week 2 Trello Files/build_db.sql
+++ Week 2 Trello Files/build_db.sql
@@ -1,37 +1,43 @@
-drop table if exists "Transactions"; 
-drop table if exists "Services";
-drop table if exists "Users_private";
-drop table if exists "Users";
+drop table if exists services;
+drop table if exists users_private;
+drop table if exists users;
+drop schema if exists user_data;
+create schema user_data;
+set search_path to user_data;
 
 
-create table "Users" (
-  "user_id" int generated always as identity primary key,
-  "Address1" varchar(255),
-  "Address2" varchar(255),
-  "City" varchar(255),
-  "State" varchar(255),
-  "PostalCode" varchar(255),
-  "Name" varchar(255) not null
+
+create table users (
+  user_id int generated always as identity,
+  username varchar(255) not null check(length(username) >=2),
+  fname varchar(255) not null,
+  lname varchar(255) not null,
+  address1 varchar(255),
+  address2 varchar(255),
+  city varchar(255),
+  state varchar(100),
+  postalcode varchar(20),
+
+  constraint users_pk
+  primary key(user_id)
+
 );
 
-create table "Users_private" (
-  "user_id" int not null,
-  "social_sn" varchar(9),
-  "password" varchar(255),
-  primary key (user_id),
-  foreign key (user_id) references "Users"(user_id)
+create table users_private (
+  user_id int not null,
+  social_sn varchar(9) check (length(social_sn) = 8),
+  password varchar(255) check (length(password) >= 8),
+  
+  constraint user_private_pk
+  primary key(user_id),
+
+  constraint user_private_fk
+  foreign key (user_id)
+  references users(user_id)
+
 );
 
-create table "Services" (
-  "service_id" int not null,
-  "description" varchar(255)
-);
-
-create table "Transactions" (
-  "transaction_id" int generated always as identity primary key,
-  "user_id" int not null,
-  "price" int not null,
-  "amount_of_service" int not null,
-  "paid" boolean not null,
-  foreign key (user_id) references "Users"(user_id)
+create table services (
+  service_id int not null,
+  description varchar(255)
 );
