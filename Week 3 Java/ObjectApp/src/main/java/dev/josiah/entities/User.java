9288f249e5c78d5578525ad233dbddd102dd0fd7
Josiah Sparks <josiah.sparks78@gmail.com>
2022-05-27 12:27:26 -0500

    replaced generated methods with Lombok annotations

--- Week 3 Java/ObjectApp/src/main/java/dev/josiah/entities/User.java
+++ Week 3 Java/ObjectApp/src/main/java/dev/josiah/entities/User.java
@@ -1,6 +1,12 @@
 package dev.josiah.entities;
 
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private int user_id;
     private String username;
@@ -11,106 +17,4 @@ public class User {
     private String city;
     private String state;
     private String postalcode;
-
-    public User() {}
-
-    public User(int user_id, String username, String fname, String lname,
-                String address1, String address2, String city, String state, String postalcode) {
-        this.user_id = user_id;
-        this.username = username;
-        this.fname = fname;
-        this.lname = lname;
-        this.address1 = address1;
-        this.address2 = address2;
-        this.city = city;
-        this.state = state;
-        this.postalcode = postalcode;
-    }
-
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFname() {
-        return fname;
-    }
-
-    public void setFname(String fname) {
-        this.fname = fname;
-    }
-
-    public String getLname() {
-        return lname;
-    }
-
-    public void setLname(String lname) {
-        this.lname = lname;
-    }
-
-    public String getAddress1() {
-        return address1;
-    }
-
-    public void setAddress1(String address1) {
-        this.address1 = address1;
-    }
-
-    public String getAddress2() {
-        return address2;
-    }
-
-    public void setAddress2(String address2) {
-        this.address2 = address2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getPostalcode() {
-        return postalcode;
-    }
-
-    public void setPostalcode(String postalcode) {
-        this.postalcode = postalcode;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "user_id=" + user_id +
-                ", username='" + username + '\'' +
-                ", fname='" + fname + '\'' +
-                ", lname='" + lname + '\'' +
-                ", address1='" + address1 + '\'' +
-                ", address2='" + address2 + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", postalcode='" + postalcode + '\'' +
-                '}';
-    }
 }
