a735e48e23111dc69508cab61630fec47e03e46a
Josiah Sparks <josiah.sparks78@gmail.com>
2022-05-27 16:03:08 -0500

    added equals and hashcode methods

--- Week 3 Java/ObjectApp/src/main/java/dev/josiah/entities/User.java
+++ Week 3 Java/ObjectApp/src/main/java/dev/josiah/entities/User.java
@@ -2,6 +2,8 @@ package dev.josiah.entities;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
@@ -17,4 +19,17 @@ public class User {
     private String city;
     private String state;
     private String postalcode;
+
+    // Usernames cannot equal by SQL constraints
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return user_id == user.getUser_id() && Objects.equals(username, user.getUsername());
+    }
+
+    @Override
+    public int hashCode() {return Objects.hash(user_id, username);}
+
 }
