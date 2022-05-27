29026e82e5a72511d8c0a0969aa2e45a01ddacfd
Josiah Sparks <josiah.sparks78@gmail.com>
2022-05-27 07:24:44 -0500

    Added most of DAO

---
+++ Week 3 Java/ObjectApp/src/main/java/dev/josiah/daos/UserDaoPostgres.java
@@ -0,0 +1,165 @@
+package dev.josiah.daos;
+
+import dev.josiah.entities.User;
+import dev.josiah.utils.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+/*
+user_id int generated always as identity,
+  username varchar(255) not null check(length(username) >=2),
+  fname varchar(255) not null,
+  lname varchar(255) not null,
+  address1 varchar(255),
+  address2 varchar(255),
+  city varchar(255),
+  state varchar(100),
+  postalcode varchar(20),
+ */
+public class UserDaoPostgres implements UserDAO{
+    final private String tn = "users";      // Table name
+    final private String c0 = "user_id";    // columns
+    final private String c1 = "username";
+    final private String c2 = "fname";
+    final private String c3 = "lname";
+    final private String c4 = "address1";
+    final private String c5 = "address2";
+    final private String c6 = "city";
+    final private String c7 = "state";
+    final private String c8 = "postalcode";
+    final private String[] c = {c0,c1,c2,c3,c4,c5,c6,c7,c8};
+
+    @Override
+    public User createUser(User user) {
+        try(Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "insert into "+tn+" values (default";
+            for (int i = 1; i<c.length;i++) { sql += ",?"; }
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getFname());
+            ps.setString(3, user.getLname());
+            ps.setString(4, user.getAddress1());
+            ps.setString(5, user.getAddress2());
+            ps.setString(6, user.getCity());
+            ps.setString(7, user.getState());
+            ps.setString(8, user.getPostalcode());
+
+            ps.execute();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            int generatedID = rs.getInt(c[0]);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        // create  new User object or modify input ref to set ID?
+        return null; // ???? Why did we generate ID ?
+    }
+
+    @Override
+    public User getUserById(int id) {
+        try(Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "select * from "+tn+" where "+c[0]+" = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1,id); // indexed from 1 rather than 0
+            ResultSet rs = ps.executeQuery();
+
+            // Get first record
+            rs.next();
+
+            User user = new User();
+            user.setUser_id(id);
+            user.setUsername(   rs.getString(c[1]));
+            user.setFname(      rs.getString(c[2]));
+            user.setLname(      rs.getString(c[3]));
+            user.setAddress1(   rs.getString(c[4]));
+            user.setAddress2(   rs.getString(c[5]));
+            user.setCity(       rs.getString(c[6]));
+            user.setState(      rs.getString(c[7]));
+            user.setPostalcode( rs.getString(c[8]));
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        //return null;
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        try(Connection conn = ConnectionUtil.getConnection()) {
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        try(Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "select * from "+tn+";";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            List<User> users = new ArrayList<User>();
+            while (rs.next()) {
+                User user = new User();
+                user.setUser_id(    rs.getInt(   c[0]));
+                user.setUsername(   rs.getString(c[1]));
+                user.setFname(      rs.getString(c[2]));
+                user.setLname(      rs.getString(c[3]));
+                user.setAddress1(   rs.getString(c[4]));
+                user.setAddress2(   rs.getString(c[5]));
+                user.setCity(       rs.getString(c[6]));
+                user.setState(      rs.getString(c[7]));
+                user.setPostalcode( rs.getString(c[8]));
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        //return null;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        try(Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "update "+tn+" set ";
+            for (int i=1; i<c.length-1;i++) {sql += c[i]+" = ?,";}
+            sql += c[c.length-1]+" = ? ;";
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getFname());
+            ps.setString(3, user.getLname());
+            ps.setString(4, user.getAddress1());
+            ps.setString(5, user.getAddress2());
+            ps.setString(6, user.getCity());
+            ps.setString(7, user.getState());
+            ps.setString(8, user.getPostalcode());
+            ps.execute();
+
+            return user;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        //return null;
+    }
+
+    @Override
+    public void deleteUserById(int id) {
+        try(Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "delete from "+tn+" where "+c[0]+" = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Week 3 Java/ObjectApp/src/main/java/dev/josiah/utils/ConnectionUtil.java
@@ -0,0 +1,20 @@
+package dev.josiah.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    public static Connection getConnection() {
+        try {
+            //String dbInfo = "jdbc:postgresql://localhost/local-db?user=postgres&password=g5rEW^";
+            //String dbinfo = System.getenv("DB_CONNECTION");
+            Connection connection = DriverManager.getConnection(System.getenv("DB_CONNECTION"));
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
