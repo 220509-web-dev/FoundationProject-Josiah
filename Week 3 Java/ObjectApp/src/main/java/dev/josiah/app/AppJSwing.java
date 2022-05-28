3e69e9264c7497c002631b0f4142ee0cbc1b5741
Josiah Sparks <josiah.sparks78@gmail.com>
2022-05-28 07:21:04 -0500

    JSwing app blank button functions added

--- Week 3 Java/ObjectApp/src/main/java/dev/josiah/app/AppJSwing.java
+++ Week 3 Java/ObjectApp/src/main/java/dev/josiah/app/AppJSwing.java
@@ -2,6 +2,7 @@ package dev.josiah.app;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class AppJSwing {
     public static void main(String[] args)
@@ -10,12 +11,34 @@ public class AppJSwing {
         JPanel panel = new JPanel();
         String[] buttonTxt = {"Register","Login","Exit"};
         panel.setLayout(new GridLayout(buttonTxt.length,1));
+        /*
         JButton[] buttons = new JButton[buttonTxt.length];
-        for (int i = 0; i < buttonTxt.length; i++)
+        for (int i = 0; i < buttons.length; i++)
         {
             buttons[i] = new JButton(buttonTxt[i]);
             panel.add(buttons[i]);
         }
+        */
+        JButton regButton = new JButton( new AbstractAction(buttonTxt[0]) {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                // register
+            }
+        });
+        JButton loginButton = new JButton( new AbstractAction(buttonTxt[1]) {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                // login
+            }
+        });
+        JButton exitButton = new JButton( new AbstractAction(buttonTxt[2]) {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                System.exit(0);
+            }
+        });
+        JButton[] buttons = {regButton, loginButton, exitButton};
+        for (int i = 0; i < buttons.length; i++) panel.add(buttons[i]);
         JOptionPane.showOptionDialog(null, panel, "Welcome!", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{}, null);
     }
 
