56d4adff520868a222ce83e4b1412c9c8ca1a25a
Josiah Sparks <josiah.sparks78@gmail.com>
2022-06-02 18:54:05 -0500

    made JDBC meet Trello reqs

--- Week 3 Java/ObjectApp/src/main/java/dev/josiah/utils/ConnectionUtil.java
+++ Week 3 Java/ObjectApp/src/main/java/dev/josiah/utils/ConnectionUtil.java
@@ -5,9 +5,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Objects;
 
+
 import static dev.josiah.complaintDepartment.ProblemScribe.Complain;
 
 public class ConnectionUtil {
+    private static ConnectionUtil instance;
+    private ConnectionUtil() { super(); }
+    public static ConnectionUtil getInstance() {
+        if(instance == null) {
+            instance = new ConnectionUtil();
+        }
+        return instance;
+    }
+    public static Connection getConnection() throws SQLException {
+        try {
+            //String dbInfo = "jdbc:postgresql://localhost/local-db?user=postgres&password=g5rEW^";
+            //String dbinfo = System.getenv("DB_CONNECTION");
+            return DriverManager.getConnection(System.getenv("DB_CONNECTION"));
+        }  catch (SQLException e) {
+            Complain(e);
+            e.printStackTrace();
+        }  catch (Throwable t) {
+            Complain(t);
+        }
+        return null;
+    }
+
+
+    /*
     private static Connection connection = null;
     static {
         //System.out.println("Making a new connection!");
@@ -23,4 +48,6 @@ public class ConnectionUtil {
         }
     }
     public static Connection getConnection() {return connection;}
+     */
+
 }
