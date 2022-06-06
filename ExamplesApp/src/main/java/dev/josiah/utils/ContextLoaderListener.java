1737e368885250e09d70f71e55f779bfaa06e24b
Josiah Sparks <josiah.sparks78@gmail.com>
2022-06-06 10:43:18 -0500

    Fixed ExamplesApp

--- ExamplesApp/src/main/java/dev/josiah/utils/ContextLoaderListener.java
+++ ExamplesApp/src/main/java/dev/josiah/utils/ContextLoaderListener.java
@@ -14,12 +14,19 @@ public class ContextLoaderListener  implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("[LOG] - The servlet context was initialized at " + LocalDateTime.now());
         ObjectMapper mapper = new ObjectMapper();
+        System.out.println("ContextLoaderListener FLAG 1");
+
         LoaderRegisteredServlet testServlet = new LoaderRegisteredServlet(mapper);
+        System.out.println("ContextLoaderListener FLAG 2");
 
         // Obtain the context from ServletContextEvent
         ServletContext context = sce.getServletContext();
-        context.addServlet("LoaderRegisteredServlet", testServlet).addMapping("/testloaded/*");
+        System.out.println("ContextLoaderListener FLAG 3");
+
+        // replaced with dynamic
+        //context.addServlet("LoaderRegisteredServlet", testServlet).addMapping("/testloaded/*");
         // Servlet registered and will handle all /* or /* anything
+        System.out.println("ContextLoaderListener FLAG 4");
 
         ServletRegistration.Dynamic registeredServlet = context.addServlet("LoaderRegisteredServlet", testServlet);
         registeredServlet.setLoadOnStartup(3);
