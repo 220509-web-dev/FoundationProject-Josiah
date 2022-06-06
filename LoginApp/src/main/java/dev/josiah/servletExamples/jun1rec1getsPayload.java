aa0b78bfcb514a45fd63dae2266b587d3ad8b747
Josiah Sparks <josiah.sparks78@gmail.com>
2022-06-05 19:28:47 -0500

    Working Jaskson parsing in Example servlet jun1rec1getsPayload

--- LoginApp/src/main/java/dev/josiah/servletExamples/jun1rec1getsPayload.java
+++ LoginApp/src/main/java/dev/josiah/servletExamples/jun1rec1getsPayload.java
@@ -1,7 +1,6 @@
 package dev.josiah.servletExamples;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.josiah.entities.Example;
 import dev.josiah.entities.User;
 
 import javax.servlet.ServletException;
@@ -26,30 +25,16 @@ public class jun1rec1getsPayload  extends HttpServlet {
         BufferedReader payloadReader = new BufferedReader(new InputStreamReader(req.getInputStream()));
 
         String line;
-        while ((line = payloadReader.readLine()) != null) System.out.println(line);
+        String payload = "";
+        while ((line = payloadReader.readLine()) != null) payload += line;
 
         try {
             System.out.println("Hello");
             ObjectMapper mapper = new ObjectMapper();
-        /*
-        User user = mapper.readValue(req.getInputStream(), User.class);
-        // Jackson takes the input stream and converts it into a User object
-        System.out.println("User's toString : ");
-        System.out.println(user);
-         */
-            Example ex = mapper.readValue(req.getInputStream(), Example.class);
-            System.out.println("Example's toString : ");
-            System.out.println(ex.toString());
-            System.out.println(ex);
-        /*
-        POST raw (type: JSON)
-{
-    "username": "Jsparks1234",
-    "password": "abc123"
-}
-        in postman to
-        http://localhost:8080/login-service/jun1rec1getsPayload
-         */
+            User user = mapper.readValue(payload, User.class);
+            // Jackson takes the input stream and converts it into a User object
+            System.out.println(user);
+
         } catch (Exception e) {
             Complain(e);
             e.printStackTrace();
