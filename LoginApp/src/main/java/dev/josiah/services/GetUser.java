673712c9a16fd2711ad19ef7880836f0b5c55e3a
Josiah Sparks <josiah.sparks78@gmail.com>
2022-06-07 21:10:56 -0500

    removed unused classes

--- LoginApp/src/main/java/dev/josiah/services/GetUser.java
+++
@@ -1,7 +0,0 @@
-package dev.josiah.services;
-
-// use this and implement once the other classes are done
-public interface GetUser {
-    // implement things for the 2 getters to do
-    // then extend this
-}
