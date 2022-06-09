23270d388b5f0f486984c8910067412d28a123db
Josiah Sparks <josiah.sparks78@gmail.com>
2022-06-08 20:29:46 -0500

    Removed comments in CORS Filter

--- LoginApp/src/main/java/dev/josiah/filters/CorsFilter.java
+++ LoginApp/src/main/java/dev/josiah/filters/CorsFilter.java
@@ -2,47 +2,27 @@ package dev.josiah.filters;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.logging.Filter;
-import java.util.logging.LogRecord;
 
 import static dev.josiah.complaintDepartment.ProblemScribe.Complain;
 
 // Registered in web.xml
-public class CorsFilter extends HttpFilter { // implements Filter
-    // Http filter is an abstract implementation of Filter
+public class CorsFilter extends HttpFilter {
 
     public void doFilter(HttpServletRequest request, HttpServletResponse res, FilterChain chain){
-        //(ServletRequest request, ServletResponse response, FilterChain chain)
-        // doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) {
         try {
-//            System.out.println("CORS Filter leveraged");
-//            if(!(res instanceof HttpServletResponse)) {
-//                chain.doFilter(request, res); return;
-//            }
-//            HttpServletResponse res = (HttpServletResponse) response; // take regular response form http server and casts to HttpServletResponse
             res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
             res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
             res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, " +
                     "Access-Control-Request-Method, Access-Control-Request-Headers");
-
             res.setHeader("Access-Control-Allow-Credentials", "true");
             chain.doFilter(request, res);
-        } catch (ServletException e) {
-            Complain(e);
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            Complain(e);
-            throw new RuntimeException(e);
+
         } catch (Throwable t) {
             Complain(t);
             throw new RuntimeException(t);
         }
     }
-
 }
