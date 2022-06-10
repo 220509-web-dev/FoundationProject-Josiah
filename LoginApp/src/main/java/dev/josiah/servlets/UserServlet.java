edb34daf0e9a58c378ea7bd62f138f02f0b37cca
Josiah Sparks <josiah.sparks78@gmail.com>
2022-06-10 04:24:17 -0500

    proper redo of GET handling

--- LoginApp/src/main/java/dev/josiah/servlets/UserServlet.java
+++ LoginApp/src/main/java/dev/josiah/servlets/UserServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import static dev.josiah.complaintDepartment.ProblemScribe.Complain;
@@ -22,9 +22,7 @@ import static dev.josiah.services.ServiceGetAllUsers.ServiceAllUsersRequest;
 public class UserServlet extends HttpServlet {
     private final static String name = "UserServlet";
     private final UserDAO userDAO;
-    private final String[] supportedURIs = new String[] {"/getall", "/getbyid", "/getbyusername"};
     private final ObjectMapper mapper = new ObjectMapper(); // TODO : add to constructor
-    private final static String self_loc = "/login-service/users";
 
     public UserServlet(UserDAO userDAO) {
         this.userDAO = userDAO;
@@ -39,136 +37,164 @@ public class UserServlet extends HttpServlet {
         System.out.println("[LOG] - Request method: " + req.getMethod());
         System.out.println("[LOG] - Request query string: " + req.getQueryString());
 
+        String potentialId = req.getParameter("id");
+        String potentialUsername = req.getParameter("username");
+        HashMap<String, Object> errorMessage = new HashMap<>();
 
-        String uri = req.getRequestURI().replace(self_loc, "");
-
-        if (!Arrays.asList(supportedURIs).contains(uri)) {
-            String complaint = "Unhandled URI posted to: " + self_loc + uri;
-            Complain(complaint);
-            System.out.println(complaint);
-            resp.setContentType("text/html");
-            resp.getWriter().write(uri + " is not a supported URI");
-            return;
-        }
-
-        if (uri.equals(supportedURIs[1])) {
-            String potentialId = req.getParameter("id");
-            String feedback;
-            if (potentialId == null) {
-                feedback = "Bad request";
-                System.out.println("Bad GET request to "+name+" with URI: "+uri.toString());
-                resp.setStatus(400);
-                resp.setContentType("text/html");
-                resp.getWriter().write(feedback); // TODO : Prepare for JS to HTML
-                return;
-            }
-            System.out.println("UserServiceServlet is forwarding to services ID : " + potentialId);
+        // TODO : Should instead be returning a webpage with the message or result nested
+        if (potentialId != null) {
+            System.out.println(potentialId+" being sent from UserServlet to the service layer");
             try {
                 User user = ServiceGetUserById.ServiceIdRequest(potentialId, userDAO);
+                resp.setStatus(200);  // unnecessary
                 resp.setContentType("application/json");
                 resp.getWriter().write(mapper.writeValueAsString(user));
                 return;
-                // TODO : prepare for JS to put in nice HTML
-
             } catch (UserNotFoundException e) {
-                feedback = "User not found";
                 resp.setStatus(404);  // ANY RESOURCE NOT FOUND IS 404
+                errorMessage.put("code", 404);
+                errorMessage.put("message", "User not found");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
             } catch (InputWasNullException e) {
-                feedback = "Form input was blank";
-                resp.setStatus(400); // BAD REQUEST, GENERAL; USER SENT DATA THAT DOESN'T MAKE SENSE
+                resp.setStatus(400);
+                errorMessage.put("code", 400);
+                errorMessage.put("message", "Form input was blank");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
             } catch (InputNotAnIntegerException e) {
-                feedback = "Input must be an integer";
                 resp.setStatus(400);
+                errorMessage.put("code", 400);
+                errorMessage.put("message", "Input must be an integer");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
             } catch (NumberFormatException e) {
-                feedback = "Please enter a whole number";
                 resp.setStatus(400);
+                errorMessage.put("code", 400);
+                errorMessage.put("message", "Please enter a whole number");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
             } catch (ValueOutOfRangeException e) {
-                feedback = "ID inputted was out of bounds";
                 resp.setStatus(400);
+                errorMessage.put("code", 400);
+                errorMessage.put("message", "ID inputted was out of bounds");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
             } catch (SQLException e) {
-                feedback = "There was a problem with the database";
                 resp.setStatus(500); // MAYBE, BUT DEPENDS ON WHAT KIND OF SQL EXCEPTION
-            } catch (Throwable t) { // happens if service or DAO layer throws anything
+                errorMessage.put("code", 500);
+                errorMessage.put("message", "There was a problem with the database");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
+            } catch (Throwable t) {
                 Complain(t);
-                feedback = "Invalid Input.";
-                resp.setStatus(400);
-            }
-            //
-            System.out.println(feedback);
-            resp.setContentType("text/html");
-            resp.getWriter().write(feedback); // TODO : Prepare for JS to HTML
-            return;
-        }
-
-        if(uri.equals(supportedURIs[2])) {
-            String potentialUsername = req.getParameter("username");
-            String feedback;
-            if (potentialUsername == null) {
-                feedback = "Bad request";
-                System.out.println("Bad GET request to "+name+" with URI: "+uri.toString());
-                resp.setStatus(400);
-                resp.setContentType("text/html");
-                resp.getWriter().write(feedback); // TODO : Prepare for JS to HTML
+                resp.setStatus(500);
+                errorMessage.put("code", 500);
+                errorMessage.put("message", "An unknown error occurred");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
                 return;
             }
-            System.out.println("UserServiceServlet is forwarding to services Username : " + potentialUsername);
-            try {
-                User user = ServiceGetUserByUsername.ServiceUsernameRequest(potentialUsername, userDAO);
-                feedback = "Got back from service layer with " + user;
-                // TODO : prepare for JS to put in nice HTML
-
-            } catch (UserNotFoundException e) {
-                feedback = "User not found";
-                resp.setStatus(404);
-            } catch (InputWasNullException e) {
-                feedback = "Form input was blank";
-                resp.setStatus(400);
-            } catch (ValueOutOfRangeException e) {
-                feedback = "Username length was incorrect";
-                resp.setStatus(400);
-            } catch (UsernameFormatException e) {
-                feedback = "Username must end with @revature.net";
-                resp.setStatus(400);
-            } catch (IllegalCharacterException e) {
-                feedback = "Username contained an illegal character";
-                resp.setStatus(400);
-            } catch (SQLException e) {
-                feedback = "There was a problem with the database";
-                resp.setStatus(500);
-            } catch (Throwable t) { // happens if service or DAO layer throws anything
-                Complain(t);
-                feedback = "Invalid Input.";
-                resp.setStatus(400);
-            }
-            System.out.println(feedback);
-            resp.setContentType("text/html");
-            resp.getWriter().write(feedback);
-            return;
         }
 
-        if(uri.equals(supportedURIs[0])) {
-            System.out.println("UserServiceServlet is forwarding to services request for all user data");
-            String feedback;
+        if(potentialUsername != null) {
+            System.out.println(potentialUsername + " being sent from UserServlet to the service layer");
             try {
-                List<User> users = ServiceAllUsersRequest(userDAO);
-                feedback = "<h2>Got back from service layer</h2>\n <p>" + users + "</p>";
-                // TODO : prepare for JS to put in nice HTML
-
+                User user = ServiceGetUserByUsername.ServiceUsernameRequest(potentialUsername, userDAO);
+                resp.setStatus(200);  // unnecessary
+                resp.setContentType("application/json");
+                resp.getWriter().write(mapper.writeValueAsString(user));
+                return;
             } catch (UserNotFoundException e) {
-                feedback = "No users exist in database";
-                resp.setStatus(204);
+                resp.setStatus(404);
+                errorMessage.put("code", 404);
+                errorMessage.put("message", "User not found");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
+            } catch (InputWasNullException e) {
+                resp.setStatus(400);
+                errorMessage.put("code", 400);
+                errorMessage.put("message", "Form input was blank");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
+            } catch (ValueOutOfRangeException e) {
+                resp.setStatus(400);
+                errorMessage.put("code", 400);
+                errorMessage.put("message", "Username length was incorrect");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
+            } catch (UsernameFormatException e) {
+                resp.setStatus(400);
+                errorMessage.put("code", 400);
+                errorMessage.put("message", "Username must end with @revature.net");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
+            } catch (IllegalCharacterException e) {
+                resp.setStatus(400);
+                errorMessage.put("code", 400);
+                errorMessage.put("message", "Username contained an illegal character");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
             } catch (SQLException e) {
-                feedback = "There was a problem with the database";
                 resp.setStatus(500);
-            }
-            catch (Throwable t) {
+                errorMessage.put("code", 500);
+                errorMessage.put("message", "There was a problem with the database");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
+            } catch (Throwable t) {
                 Complain(t);
-                feedback = "An internal error occurred.";
                 resp.setStatus(500);
+                errorMessage.put("code", 500);
+                errorMessage.put("message", "An unknown error occurred");
+                errorMessage.put("timestamp", LocalDateTime.now().toString());
+                resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+                return;
             }
-            System.out.println(feedback);
-            resp.setContentType("text/html");
-            resp.getWriter().write(feedback);
+        }
+
+
+        System.out.println("UserServlet is forwarding to services request for all user data");
+        try {
+            List<User> users = ServiceAllUsersRequest(userDAO);
+            resp.setStatus(200);  // unnecessary
+            resp.setContentType("application/json");
+            resp.getWriter().write(mapper.writeValueAsString(users));
+            return;
+        } catch (UserNotFoundException e) {
+            resp.setStatus(204);
+            HashMap<String, Object> message = new HashMap<>();
+            message.put("code", 204);
+            message.put("message", "No users exist in database");
+            message.put("timestamp", LocalDateTime.now().toString());
+            resp.getWriter().write(mapper.writeValueAsString(message));
+            return;
+        } catch (SQLException e) {
+            resp.setStatus(500);
+            errorMessage.put("code", 500);
+            errorMessage.put("message", "There was a problem with the database");
+            errorMessage.put("timestamp", LocalDateTime.now().toString());
+            resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+            return;
+        } catch (Throwable t) {
+            Complain(t);
+            resp.setStatus(500);
+            errorMessage.put("code", 500);
+            errorMessage.put("message", "An unknown error occurred");
+            errorMessage.put("timestamp", LocalDateTime.now().toString());
+            resp.getWriter().write(mapper.writeValueAsString(errorMessage));
+            return;
         }
     }
 }
